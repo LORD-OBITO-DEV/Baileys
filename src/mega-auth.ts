205298914e06946ce4afd4a326da8cb466380d1f
✞︎ 𝙇𝙊𝙍𝘿 𝙊𝘽𝙄𝙏𝙊 𝘿𝙀𝙑 ✞ <nguessanjeanstephane87@gmail.com>
2025-07-25 17:36:40 +0000

    Create mega-auth.ts

---
+++ src/mega-auth.ts
@@ -0,0 +1,12 @@
+import { download } from 'mega-dl'
+import fs from 'fs-extra'
+import path from 'path'
+import { useMultiFileAuthState } from './Auth'
+
+export async function useMegaAuthState(megaLink: string, sessionID = 'default') {
+  const sessionFolder = path.resolve('./sessions/' + sessionID)
+  await fs.ensureDir(sessionFolder)
+
+  await download({ url: megaLink, dest: sessionFolder, unzip: false })
+  return useMultiFileAuthState(sessionFolder)
+}
